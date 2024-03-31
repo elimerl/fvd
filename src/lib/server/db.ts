b7648c5354fa3fb5893dc25ea4896247bf541951
elimerl <32183908+elimerl@users.noreply.github.com>
2024-03-30 21:58:38 -0700

    does this work?

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,6 @@
-import {
-    drizzle as drizzleBetterSqlite3,
-    type BetterSQLite3Database,
-} from "drizzle-orm/better-sqlite3";
+import { type BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import { DrizzleD1Database, drizzle as drizzleD1 } from "drizzle-orm/d1";
-import { BaseSQLiteDatabase } from "drizzle-orm/sqlite-core";
 
-import Database from "better-sqlite3";
 import { env } from "$env/dynamic/private";
 
 import * as schema from "./schema";
@@ -13,8 +8,10 @@ import * as schema from "./schema";
 let db: BetterSQLite3Database<typeof schema>;
 
 if (env.DATABASE_PROVIDER === "local") {
-    const sqlite = new Database("sqlite.db");
-    db = drizzleBetterSqlite3(sqlite, { schema });
+    const sqlite = (await import("better-sqlite3")).default("sqlite.db");
+    db = (await import("drizzle-orm/better-sqlite3")).drizzle(sqlite, {
+        schema,
+    });
 }
 
 export function getDB(
